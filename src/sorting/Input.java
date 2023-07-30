09ca6a66503a9c52309137b0898088212759dac3
saivishwatej <saivishwatejreddychitta@SAIVISHATEJsMBP.Dlink>
2023-07-30 17:29:27 +0530

    initial commit

---
+++ src/sorting/Input.java
@@ -0,0 +1,23 @@
+package sorting;
+
+import java.time.Duration;
+import java.time.Instant;
+
+
+public class Input {
+    public static int[] get_Unsorted_array(){
+        return new int[]{5, 6, 1, 0, 3};
+    }
+
+    public void print(int[] array, String str, Instant start, Instant end){
+        if(start != null && end !=null){
+            Duration timeElapsed = Duration.between(start, end);
+            System.out.println("Time taken: "+ timeElapsed.toMillis() +" milliseconds");
+        }
+        System.out.print(str + ": ");
+        for (int j : array) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+    }
+}
